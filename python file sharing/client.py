f4ef059f0749d6994cfc0e00157aa68f0ac9dbf4
DSON-MacEmu <sondaniel96@gmail.com>
2017-11-27 23:25:28 -0800

    client WIP

--- python file sharing/client.py
+++ python file sharing/client.py
@@ -1,6 +1,7 @@
-import socket, os, datetime
+import socket, os, datetime, json
 
 IP = '192.168.1.8'
+MY_IP = '192.168.1.11'
 PORT = 3000
 FILE_TRANSFER_PORT = 3001
 FILE_PATH = '../sync/'
@@ -12,13 +13,18 @@ def get_diff(obj_1, obj_2):
     diff = {}
     for key in obj_1.keys():
         value = obj_1[key]
-        if key not in a:
+        if key not in obj_2.keys():
             diff[key] = value
         else:
-            if a[key] != value:
+            if obj_2[key] != value:
                 diff[key] = value
 
     return diff
+
+def datetime_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    raise TypeError("Unknown type")
 
 def get_files():
     print('get_files function running.')
@@ -38,54 +44,38 @@ try:
     # Try to connect to other ends discovery port 
     my_socket.connect((IP , PORT))
 
-    # Send FILE_TRANSFER_PORT to ip
-    my_socket.sendall(str(FILE_TRANSFER_PORT).encode())
-
-    # create file socket
-    file_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    file_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    try:
-        my_socket.recv(1024)
-        # try to connect to other end file socket
-        file_socket.connect((IP, FILE_TRANSFER_PORT))
-
-        print("connected to %s:%s File Transfer Socket" % (IP, FILE_TRANSFER_PORT))
-        
-        # Recieve data
-        data = file_socket.recv(1024).decode()
-        
-        # Send all data
-        file_socket.sendall(str({
-            'ips': localnet_ips,
-            'files': hash_files
-            }).encode())
-
-        # Calculate differences
-        ips_diff = set(localnet_ips) - set(data['ips'])
-        file_diff = get_diff(hash_files, data['files'])
-
-        # Send differences
-        file_socket.sendall(str({
-            'ips_diff' : ips_diff,
-            'file_diff': file_diff
+    # Send all data
+    my_socket.sendall(json.dumps({
+        'ips': localnet_ips,
+        'files': json.dumps(hash_files, default=datetime_handler)
         }).encode())
-
-        data_diff = file_socket.recv(1024).decode()
-
-        ##########################################################
-        #                Logic to receive file                   #
-        ##########################################################
-        for fileName in data_diff['file_diff']:
-            file_data = file_socket.recv(1024)
-            downloadFile = open((FILE_PATH, fileName), 'wb')
-            while file_data:
-                downloadFile.write(file_data)
-                file_data = file_socket.recv(1024)
         
+    # Recieve data
+    data = json.loads(my_socket.recv(1024).decode())
 
-    except socket.error as e:
-        print("File Transfer Connection to %s:%s failed: %s" % (IP, FILE_TRANSFER_PORT, e))
+
+
+    # Calculate differences
+    ips_diff = set(localnet_ips) - set(data['ips'])
+    file_diff = get_diff(hash_files, json.loads(data['files']))
+
+    # Send differences
+    my_socket.sendall(json.dumps({
+        'ips_diff' : ips_diff,
+        'file_diff': json.dumps(file_diff, default=datetime_handler)
+    }).encode())
+
+    data_diff = json.loads(my_socket.recv(1024).decode())
+
+    ##########################################################
+    #                Logic to receive file                   #
+    ##########################################################
+    for fileName in json.loads(data_diff['file_diff']).keys():
+        file_data = my_socket.recv(1024)
+        downloadFile = open((FILE_PATH, fileName), 'wb')
+        while file_data:
+            downloadFile.write(file_data)
+            file_data = my_socket.recv(1024)
 
 except socket.error as err:
     print("Discovery Connection to %s:%s failed: %s" % (IP, PORT, err))
